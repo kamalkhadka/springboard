97995fb231d4f5a9f7aad97574233a518271e50a
kamal-khadka <38675025+kamal-khadka@users.noreply.github.com>
2020-05-24 18:08:50 -0700

    stored responses in flask session

--- 19.Flask/exercise/flask-survey/app.py
+++ 19.Flask/exercise/flask-survey/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
@@ -7,17 +7,24 @@ app.config['SECRET_KEY'] = "chicken"
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 debug = DebugToolbarExtension(app)
 
-responses = []
+# @app.before_request
+# def test():
+#     print(f"{session['responses']}")
 
 @app.route("/")
 def root():
     survey_title = satisfaction_survey.title
     return render_template("/index.html", survey_title = survey_title)
 
+@app.route("/start", methods=["POST"])
+def start_survey():
+    session["responses"] = []
+    return redirect(f"/questions/{len(session['responses'])}")
+
 @app.route("/questions/<int:id>")
 def get_question(id):
     
-    responses_length = len(responses)
+    responses_length = len(session["responses"])
     if id == responses_length:
         question = satisfaction_survey.questions[id].question
         choices = satisfaction_survey.questions[id].choices
@@ -31,7 +38,9 @@ def get_question(id):
 @app.route("/answer", methods=["POST"])
 def answer():
     answer = request.form["choice"]
-    responses.append(answer)    
+    responses = session["responses"]
+    responses.append(answer)
+    session["responses"] = responses    
     if len(satisfaction_survey.questions) > len(responses):
         return redirect(f"/questions/{len(responses)}")
     return redirect("/thank-you")
