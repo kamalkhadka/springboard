da5be9db9e4fef3780df504bd1166620c088357f
kamal-khadka <38675025+kamal-khadka@users.noreply.github.com>
2020-06-09 11:38:17 -0700

    dunder __repr__ added model

--- 23.SQLAlchemy/models.py
+++ 23.SQLAlchemy/models.py
@@ -13,7 +13,13 @@ class Pet(db.Model):
     """Pet"""
     __tablename__ = 'pets'
 
+    def __repr__(self):
+        """Show info about pet."""
+        p = self
+        return f"<Pet {p.id} {p.name} {p.species} {p.hunger}>"
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50), nullable=False, unique=True)
     species = db.Column(db.String(30))
     hunger = db.Column(db.Integer, nullable=False, default=20)
+
