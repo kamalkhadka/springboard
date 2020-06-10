fc14c6c14621907e59f9db9fe8dbead25c7c2dfe
kamal-khadka <38675025+kamal-khadka@users.noreply.github.com>
2020-06-09 17:21:49 -0700

    flask sql alchemy crud

--- 23.SQLAlchemy/app.py
+++ 23.SQLAlchemy/app.py
@@ -16,5 +16,29 @@ connect_db(app)
 
 @app.route('/')
 def home_page():
-    """Shows home page"""
-    return render_template('home.html')
+    """Shows list of all pets in db"""
+    pets = Pet.query.all()
+    return render_template('list.html', pets = pets)
+
+@app.route('/', methods=['POST'])
+def create_pet():
+    form = request.form
+    name = form['name']
+    species = form['species']
+    hunger = int(form['hunger'])
+    new_pet = Pet(name=name, species=species, hunger=hunger)
+    db.session.add(new_pet)
+    db.session.commit()
+    return redirect(f'/pet/{new_pet.id}')
+
+@app.route('/pet/<int:id>')
+def show_pet(id):
+    """Show details about a pet"""
+    pet = Pet.query.get_or_404(id)
+    return render_template('details.html', pet=pet)
+
+@app.route('/species/<species_id>')
+def show_pets_by_species(species_id):
+    """Get all pets by species"""
+    pets = Pet.get_by_species(species_id)
+    return render_template('species.html', pets=pets, species=species_id)
--- 23.SQLAlchemy/models.py
+++ 23.SQLAlchemy/models.py
@@ -13,13 +13,31 @@ class Pet(db.Model):
     """Pet"""
     __tablename__ = 'pets'
 
+    @classmethod
+    def get_by_species(cls, species):
+        """ Get all pets for species"""
+        return cls.query.filter_by(species = species).all()
+
+    @classmethod
+    def get_all_hungry(cls):
+        """ Get all pets where hunger > 20 """
+        return cls.query.filter(Pet.hunger > 20).all()
+
     def __repr__(self):
         """Show info about pet."""
         p = self
-        return f"<Pet {p.id} {p.name} {p.species} {p.hunger}>"
+        return f"<Pet id={p.id} name={p.name} species={p.species} hunger={p.hunger}>"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50), nullable=False, unique=True)
     species = db.Column(db.String(30))
     hunger = db.Column(db.Integer, nullable=False, default=20)
+
+    def greet(self):
+        return f"Hi, I am {self.name} the {self.species}"
+
+    def feed(self, amt=20):
+        """update hunger based off of amount"""
+        self.hunger -= amt
+        self.hunger = max(self.hunger, 0)
 
