ff8e4d403b383a837cb869e8da16303a937c05f4
kamal-khadka <38675025+kamal-khadka@users.noreply.github.com>
2020-06-16 18:02:02 -0700

    flask wtf forms

---
+++ 24.IntermediateFlask/forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, BooleanField, IntegerField, RadioField, SelectField
+from wtforms.validators import InputRequired, Email, Optional
+
+
+class AddSnackForm(FlaskForm):
+
+    email = StringField('Email', validators=[Optional(), Email()])
+    name = StringField('Snack Name', validators=[InputRequired(message='Snack name can\'t be blank')])
+    price = FloatField('Price in USD')
+    is_healthy = BooleanField('This is a healthy snack')
+    quantity = IntegerField('How many')
+
+    category = SelectField('Category', 
+                          choices=[('ic', 'Ice Cream'), ('chips', 'Potato Chips'), ('candy', 'Candy/Sweets')])
+
+
+class NewEmployeeForm(FlaskForm):
+    """ WTForm to add new employee """
+    name = StringField('Employee Name')
+    state = StringField('State')
+    dept_code = SelectField('Department Code')
