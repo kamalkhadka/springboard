5622015118884d228194148e4832fcd0082f47a6
kamal-khadka <38675025+kamal-khadka@users.noreply.github.com>
2020-08-06 16:20:34 -0700

    testing with jest

---
+++ 31.NodeFundamentals/testing/matchers.test.js
@@ -0,0 +1,34 @@
+describe('test matches', function(){
+    test('compare toBe and toEqual', function(){
+        const nums = [3,4,5];
+        const copy = [...nums];
+        const nums2 = nums
+        expect(copy).toEqual(nums);
+        expect(nums2).toBe(nums);
+    })
+
+    test('playing with toContain matcher', function(){
+        const colors = ['red', 'orange']
+        expect(colors).toContain('red')
+        expect('hello').toContain('hell')
+    })
+
+    test('playing with numeric matchers', function(){
+        expect(7).toBeGreaterThanOrEqual(2);
+    })
+
+    test('playing with boolean matchers', function(){
+        expect('hi').toBeTruthy();
+    })
+
+    test('playing with any', function(){
+        const randNum = Math.random() * 6;
+        expect(randNum).toEqual(expect.any(Number))
+    })
+
+    test('playing with not', function(){
+        const numLives = 9;
+        expect(numLives).not.toEqual(0);
+    })
+
+})
---
+++ 31.NodeFundamentals/testing/square.js
@@ -0,0 +1,12 @@
+
+function square(x){
+    return x * x;
+}
+
+function cube(x){
+    return Math.pow(x, 3);
+}
+
+module.exports = {
+    square, cube
+}
