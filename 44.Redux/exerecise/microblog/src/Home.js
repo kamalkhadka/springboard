0f92f2be214f7b2795e8f4c9db99fa87e11d7917
kamal-khadka <38675025+kamal-khadka@users.noreply.github.com>
2020-11-10 11:08:15 -0700

    redux exercise

--- 44.Redux/exerecise/microblog/src/Home.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import PostList from "./PostList";
-
-const Home = () => {
-  return <PostList />;
-};
-
-export default Home;
--- 44.Redux/exerecise/microblog/src/blogContext.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const BlogContext = React.createContext();
-
-export default BlogContext;
